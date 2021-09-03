30e28fc6482291174e546bf3ba7aca42c821b747
Luis G. Villegas <luisguve@gmail.com>
2021-09-02 22:56:59 -0300

    Se introdujo el punto resumen para obtener los ejercicios de muestra de la categoria

--- api/categoria/controllers/categoria.js
+++ api/categoria/controllers/categoria.js
@@ -8,8 +8,8 @@ const { sanitizeEntity } = require('strapi-utils');
  */
 
 module.exports = {
-  /*
-  * Retorna las categorias y sus subcategorias sin los ejercicios
+  /**
+  * Retorna las categorias raiz sin los ejercicios
   */
   async find(ctx) {
     let entities;
@@ -53,21 +53,61 @@ module.exports = {
 
     const entity = await strapi.services.categoria.findOne({ Titulo_url: slug });
     const categoria = sanitizeEntity(entity, { model: strapi.models.categoria });
-    categoria.ejercicios = categoria.ejercicios.length;
+    categoria.ejercicios = categoria.ejercicios.map(e => e.slug);
+    delete categoria.padre
 
     if (categoria.hijos) {
       await eachHijo(categoria.hijos, async (c) => {
         const categoria = await strapi.services.categoria.findOne({ id: c.id });
         c = sanitizeEntity(categoria, { model: strapi.models.categoria });
 
-        if (c.ejercicios) {
-          c.ejercicios = c.ejercicios.length
-        }
+        c.ejercicios = c.ejercicios.map(e => e.slug)
+
         delete c.padre
         return c
       })
     }
     return categoria;
+  },
+  /**
+  * Retorna los ejercicios de muestra de la categoria junto con el numero total de
+  * ejercicios en las subcategorias.
+  */
+  async resumen(ctx) {
+    const { slug } = ctx.params;
+
+    const entity = await strapi.services.categoria.findOne({ Titulo_url: slug });
+    const categoria = sanitizeEntity(entity, { model: strapi.models.categoria });
+    // Cuenta los ejercicios de la categoria
+    let q = categoria.ejercicios.length;
+
+    // Obtiene los ejercicios de muestra
+    const muestras = categoria.muestras.map(e => {
+      const ejercicio = sanitizeEntity(e, { model: strapi.models.ejercicio });
+      if (ejercicio.solucion) {
+        delete ejercicio.solucion;
+      }
+      if (ejercicio.solucion_pdf) {
+        delete ejercicio.solucion_pdf;
+      }
+      return ejercicio;
+    });
+    // Cuenta los ejercicios de las subcategorias
+    if (categoria.hijos) {
+      await eachHijo(categoria.hijos, async (subcategoria) => {
+        const entity = await strapi.services.categoria.findOne({ id: subcategoria.id });
+        subcategoria = sanitizeEntity(entity, { model: strapi.models.categoria });
+
+        if (subcategoria.ejercicios) {
+          q += subcategoria.ejercicios.length
+        }
+        return subcategoria
+      })
+    }
+    return {
+      muestras,
+      q,
+    };
   }
 };
 
