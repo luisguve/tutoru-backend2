f73078939890e9f658cd00635f5bf047564cfa2f
Luis G. Villegas <luisguve@gmail.com>
2021-09-07 21:06:57 -0300

    Construye el arbol de categorias en el handler de comprados

--- api/ejercicio/controllers/ejercicio.js
+++ api/ejercicio/controllers/ejercicio.js
@@ -92,7 +92,7 @@ module.exports = {
     const usuario = await strapi.services["usuarios-ejercicios"].findOne({ user_id: id })
     if (!usuario || !usuario.ejercicios || !usuario.ejercicios.length) return null
 
-    return usuario.ejercicios.map(entity => {
+    return await Promise.all(usuario.ejercicios.map(async entity => {
       const ejercicio = sanitizeEntity(entity, { model: strapi.models.ejercicio });
       if (ejercicio.solucion) {
         delete ejercicio.solucion;
@@ -100,8 +100,21 @@ module.exports = {
       if (ejercicio.solucion_pdf) {
         delete ejercicio.solucion_pdf;
       }
+      // Antes de construir  el arbol de categorias, reasigna la categoria completa
+      // ya que solo contiene su id.
+      const categoria = await strapi.services.categoria.findOne({
+        id: ejercicio.categoria
+      });
+      ejercicio.categoria = categoria
+      // Anexa el arbol de categorias al ejercicio
+      ejercicio.arbolCategorias = await this.construirArbolCategoria(ejercicio);
+      // El ejercicio tiene la categoria completa.
+      // Asigna el titulo de la categoria en lugar de la categoria completa.
+      ejercicio.categoria = {
+        Titulo_normal: categoria.Titulo_normal
+      }
       return ejercicio;
-    });
+    }));
   },
   /**
   * Retorna los IDs de los ejercicios que ha adquirido el usuario
@@ -113,5 +126,19 @@ module.exports = {
     if (!usuario || !usuario.ejercicios || !usuario.ejercicios.length) return null
 
     return usuario.ejercicios.map(e => e.id)
+  },
+  /**
+  * Construye el arbol de categorias como un array, comenzando por la categoria raiz.
+  */
+  async construirArbolCategoria(ejercicio) {
+    let categoriaActual = ejercicio.categoria;
+    const arbol = [categoriaActual.Titulo_url];
+    while (categoriaActual.padre) {
+      categoriaActual = await strapi.services.categoria.findOne({
+        id: categoriaActual.padre.id || categoriaActual.padre
+      });
+      arbol.unshift(categoriaActual.Titulo_url);
+    }
+    return arbol
   },
 };
