7e093355b6fe59167ace290dc4f3dc660e65efce
Luis G. Villegas <luisguve@gmail.com>
2021-12-06 15:18:11 -0400

    Setting up handlers for posting responses to comments

--- plugins/masterclass/controllers/respuesta.js
+++ plugins/masterclass/controllers/respuesta.js
@@ -5,4 +5,22 @@
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async create(ctx) {
+    const { user } = ctx.state
+    const { id } = ctx.params
+    const pregunta = await strapi.query("comentario", "masterclass").findOne({id})
+    if (!pregunta) {
+      return ctx.throw(404)
+    }
+    const { contenido } = ctx.request.body
+    const nuevaRespuesta = await strapi.query("respuesta", "masterclass").create({
+      autor: user.id,
+      contenido,
+      comentario: id
+    })
+    return {
+      id: nuevaRespuesta.id
+    }
+  }
+};
