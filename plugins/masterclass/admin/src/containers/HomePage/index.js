c1c1b1c4b052c7136fd320dd3d0150bbf3bd1a5c
Luis G. Villegas <luisguve@gmail.com>
2021-12-06 15:18:14 -0400

    Updated plugin's homepage

--- plugins/masterclass/admin/src/containers/HomePage/index.js
+++ plugins/masterclass/admin/src/containers/HomePage/index.js
@@ -10,8 +10,9 @@ const HomePage = () => {
   return (
     <div className="pt-5 px-4">
       <div className="bg-white shadow-sm d-flex flex-column py-3 px-5">
-        <h1>Masterclass</h1>
-        <p>Hello friendss</p>
+        <h1>Masterclass plugin</h1>
+        <p>Work in progress</p>
+        <p className="small">Contact me: luisguveal@gmail.com</p>
       </div>
     </div>
   );
